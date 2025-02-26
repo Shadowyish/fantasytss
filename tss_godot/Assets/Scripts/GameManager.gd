d3467d799afeb6bbf78f00ec6cc3e0ce15e59c21
Moth <Shadowyish0@gmail.com>
2025-02-26 15:43:29 -0500

    Intial Player controller

---
+++ tss_godot/Assets/Scripts/GameManager.gd
@@ -0,0 +1,4 @@
+extends Node
+
+enum ControlType { MOUSE_KEYBOARD, GAMEPAD }
+var input_type = ControlType.MOUSE_KEYBOARD  # Default to keyboard & mouse
---
+++ tss_godot/Assets/Scripts/Weapon.gd
@@ -0,0 +1,5 @@
+extends Node
+class_name Weapon
+
+func attack():
+    push_error("Attack not implemented in: " + str(self))
