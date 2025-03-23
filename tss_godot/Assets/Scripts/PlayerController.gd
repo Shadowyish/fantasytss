54cb1d28a7b39f08b06e65a65a08d5b601376fde
Shadowyish <Shadowyish0@gmail.com>
2025-03-23 18:01:13 -0400

    Added Animations to Player, changed prefab to Warrior

--- tss_godot/Assets/Scripts/PlayerController.gd
+++ tss_godot/Assets/Scripts/PlayerController.gd
@@ -7,22 +7,29 @@ extends CharacterBody2D
 @export var mana: int = 20
 @export var cur_weapon: Node
 
-var is_dead = false
+
 var cur_health: int = health
 var aim_direction = Vector2.RIGHT
 var iframe_time: float = 0.67
 var is_invunerable = false
+var is_dead = false
+var is_hit = false
+var is_facing_right = true
 
 signal player_died
 
 #Using a timer for iframes to make it unblocking
 @onready var iframe_timer = Timer.new()
+@onready var anim = $Animator
 
 func _ready():
 	# timer hookups
 	add_child(iframe_timer)
 	iframe_timer.one_shot = true
 	iframe_timer.connect("timeout", _on_iframe_timeout)
+	
+	# hit animation hookup
+	anim.animation_finished.connect(_on_animation_finished)
 	
 	GameManager.player = self
 
@@ -36,16 +43,17 @@ func _physics_process(delta):
 	
 	velocity = input_vector * speed * delta
 	move_and_slide()
-
-	# Get rotation input and rotate char
+	
+	# Get rotation direction
 	if GameManager.game_manager.input_type == GameManager.ControlType.MOUSE_KEYBOARD:
 		aim_direction = (get_global_mouse_position() - global_position).normalized()
 	elif GameManager.game_manager.input_type == GameManager.ControlType.GAMEPAD:
-		var right_stick = Vector2(
+		aim_direction = Vector2(
 			Input.get_axis("right_stick_left", "right_stick_right"),
 			Input.get_axis("right_stick_up", "right_stick_down")
 		).normalized()
-	rotation = lerp(rotation, aim_direction.angle(), rotation_speed * delta)
+	
+	play_cur_animation()
 
 func take_damage(damage: int):
 	if is_dead:
@@ -55,7 +63,8 @@ func take_damage(damage: int):
 		die()
 	else: # start iframes
 		is_invunerable = true
-		#TODO: give some indication of iframes
+		is_hit = true
+		anim.play("hit_right" if is_facing_right else "hit_left")
 		#start timer
 		iframe_timer.start(iframe_time)
 
@@ -64,14 +73,25 @@ func use_weapon():
 
 func die():
 	emit_signal("player_died")
-	play_death_animation()
+	anim.play("death_right" if is_facing_right else "death_left")
 	await(get_tree().create_timer(2.0))
 	#TODO: Trigger GameOver/Score Screen
 
-func play_death_animation():
-	#TODO: Play Death Animation?
-	pass
-
 func _on_iframe_timeout():
 	is_invunerable = false
-	#TODO: remove visuals
+
+func _on_animation_finished():
+	if is_hit:
+		is_hit = false
+		play_cur_animation()
+
+#Play Correct animation based on facing
+func play_cur_animation():
+	#determine direction
+	is_facing_right = aim_direction.x >= 0
+	
+	if not is_hit: #Don't change animation if playing hit animation
+		if velocity.length() == 0:
+			anim.play("idle_right" if is_facing_right else "idle_left")
+		else:
+			anim.play("run_right" if is_facing_right else "run_left")
