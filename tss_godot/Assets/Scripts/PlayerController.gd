38e3fb48414a1d06af90d0850589c94acb0579c9
Shadowyish <Shadowyish0@gmail.com>
2025-03-23 20:19:19 -0400

    Changed names for input actions

--- tss_godot/Assets/Scripts/PlayerController.gd
+++ tss_godot/Assets/Scripts/PlayerController.gd
@@ -40,8 +40,8 @@ func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	# Get movement input then normalize to prevent speed boost
 	input_vector = Vector2(
-		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
+		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	).normalized()
 	
 	velocity = input_vector * speed * delta
