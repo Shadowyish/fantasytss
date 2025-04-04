b21e75cac494f127ddb351fae9810776feef53b3
Moth <Shadowyish0@gmail.com>
2025-04-04 00:06:00 -0400

    pushing to pc

---
+++ tss_godot/Assets/Scripts/MainMenuController.gd
@@ -0,0 +1,18 @@
+extends Control
+
+func _ready():
+	# Connect button signals
+	$VBoxContainer/StartButton.pressed.connect(_on_start_game_pressed)
+	$VBoxContainer/OptionsButton.pressed.connect(_on_options_pressed)
+	$VBoxContainer/QuitButton.pressed.connect(_on_quit_pressed)
+
+func _on_start_game_pressed():
+	# Pass loading logic to the Game Manager
+	GameManager.launch_game("OvalForest.tscn")
+
+func _on_options_pressed():
+	#TODO:Implement Options?
+	print("Options Menu - To Be Implemented")
+
+func _on_quit_pressed():
+	get_tree().quit()  # Closes the game
