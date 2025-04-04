3944177a85b4132b1bbcb0a6ce6520c4286a44bd
Shadowyish <Shadowyish0@gmail.com>
2025-04-04 19:05:35 -0400

    Added sprites and animation logic to monsters

--- tss_godot/Assets/Scripts/EnemyController.gd
+++ tss_godot/Assets/Scripts/EnemyController.gd
@@ -7,8 +7,13 @@ extends CharacterBody2D
 
 var player = null
 var is_dead = false
+var is_hit = false
+var is_spawning = true
+var is_facing_right = true
+var direction = Vector2.RIGHT
 
 @onready var detection_area = $CollisionShape2D
+@onready var anim = $Animator
 
 func _ready():
 	#get player reference
@@ -20,8 +25,12 @@ func _physics_process(delta):
 		return
 	if is_dead:
 		return
+	if is_spawning:
+		await(anim.animation_finished)
+		is_spawning = false
+		return
 	
-	var direction = (player.global_position - global_position).normalized()
+	direction = (player.global_position - global_position).normalized()
 	# Move towards the player
 	velocity = direction * speed * delta
 	var collision = move_and_collide(velocity)
@@ -31,6 +40,8 @@ func _physics_process(delta):
 		var body = collision.get_collider()
 		if body.is_in_group("Player"):
 			body.take_damage(damage)
+	
+	play_cur_animation()
 
 func take_damage(dmg: int):
 	if is_dead:
@@ -41,16 +52,22 @@ func take_damage(dmg: int):
 	hp -= dmg
 	if hp <= 0:
 		die()
+		return
+	is_hit = true
+	anim.play("hit_right" if is_facing_right else "hit_left")
 
 func die():
 	is_dead = true
-	play_death_animation()
 	GameManager.increase_score(score)
 	GameManager.enemy_count -= 1
-	
-	await(get_tree().create_timer(.25).timeout)
+	anim.play("death_right" if is_facing_right else "death_left")
+	await(anim.animation_finished)
 	queue_free() #Remove from scene
 
-func play_death_animation():
-	# TODO: Play Death Animation/Effect
-	pass
+func play_cur_animation():
+	if is_hit or is_dead: #Don't change animation if playing another animation
+		return
+	#determine direction
+	is_facing_right = direction.x >= 0
+	
+	anim.play("run_right" if is_facing_right else "run_left")
