3e8f9189bfc2942feea909a05b1ebf78148aa6aa
Shadowyish <Shadowyish0@gmail.com>
2025-04-04 22:18:00 -0400

    Game UI Now displays difficulty thresholds

--- tss_godot/Assets/Scripts/UIOverlay.gd
+++ tss_godot/Assets/Scripts/UIOverlay.gd
@@ -2,6 +2,7 @@ extends Control
 
 @onready var hp_label = $CanvasLayer/ColorRect/HP
 @onready var score_label = $CanvasLayer/ColorRect/Score
+@onready var difficulty_threshold_label = $CanvasLayer/ColorRect/Difficulty
 @onready var pause_panel = $CanvasLayer/PausePanel
 @onready var resume_button = $CanvasLayer/PausePanel/VBoxContainer/ResumeButton
 @onready var quit_button = $CanvasLayer/PausePanel/VBoxContainer/QuitButton
@@ -13,6 +14,7 @@ func _ready():
 func _process(_delta):
 	hp_label.text =  "HP: " + str(GameManager.player.cur_health) if GameManager.player.cur_health > 0 else "HP: DEAD"
 	score_label.text = "Current Score: " + str(GameManager.game_score)
+	difficulty_threshold_label.text = "Next Difficulty Threshold: " + str(GameManager.next_threshold_cap)
 	if GameManager.game_mode == GameManager.GameMode.Pause:
 		pause_panel.visible = true
 	
