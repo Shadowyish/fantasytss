5ce5e7ca6e95763a7e9bdd9f072e34a428fbd5c8
Shadowyish <Shadowyish0@gmail.com>
2025-04-04 21:08:14 -0400

    added Wraith to spawn, made difficulty increase as score goes up

--- tss_godot/Assets/Scripts/GameManager.gd
+++ tss_godot/Assets/Scripts/GameManager.gd
@@ -9,21 +9,26 @@ var camera: Camera2D #Spawn into scene instead of natively in scene
 var follow_speed: float = 5.0
 var time_passed: float = 0.0
 var game_score: int = 0
+var score_per_second: int = 5
 var enemy_count: int = 0
-var spawn_offset: int = -150 # How many pixels offset from the camera border spawns for enemies should occur
+var enemy_max: int = 10 # Max enemies at one time, difficulty increases this
+var spawn_offset: int = -50 # How many pixels offset from the camera border spawns for enemies should occur
+var next_threshold_cap: int = 2500 # the score at which the game first increases difficulty
 var is_playing = false
 
 func _process(delta):
 	if(game_mode == GameMode.Game):
 		# camera panning: Smooth follows player
 		camera.position = camera.position.lerp(GameManager.player.position, follow_speed * delta)
-		if enemy_count < 10:
+		if enemy_count < enemy_max:
 			spawn_enemies()
 		if player.cur_health > 0:
 			time_passed += delta
 			while(time_passed > 1.0):
 				time_passed -= 1
-				increase_score(5)
+				increase_score(score_per_second)
+		if game_score > next_threshold_cap:
+			increase_difficulty()
 	
 func launch_game(map: String):
 	get_tree().change_scene_to_file("res://Assets/Scenes/"+ map)
@@ -55,15 +60,7 @@ func spawn_enemies():
 		3: #Bottom
 			spawn_position = Vector2(randf_range(camera.global_position.x - viewport_size.x / 2, camera.global_position.x + viewport_size.x / 2), 
 									 camera.global_position.y + viewport_size.y / 2 + spawn_offset)
-	
-	#Temp Logic for spawning a skelton or zombie
-	# TODO: Build proper list of spawns, that way we can have more than two
-	var enemy_rand = randi() % 6
-	var enemy = null
-	if enemy_count % 7 >= 7 - enemy_rand:
-		enemy = load("res://Assets/Prefabs/Zombie.tscn").instantiate()
-	else:
-		enemy = load("res://Assets/Prefabs/Skeleton.tscn").instantiate()
+	var enemy = get_next_enemy()
 	enemy.position = spawn_position
 	get_tree().current_scene.add_child(enemy)
 	enemy_count += 1
@@ -72,3 +69,32 @@ func increase_score(score: int):
 	game_score += score
 	push_warning("Score added: " + str(score))
 	push_warning("Score now totals: " + str(game_score))
+	
+func increase_difficulty():
+	enemy_max += 2
+	spawn_offset -= 5
+	score_per_second += 5
+	next_threshold_cap += next_threshold_cap * .5 + 1250
+	
+func get_next_enemy():
+	#Spawn only skeletons at the start
+	if next_threshold_cap <= 2500:
+		return load("res://Assets/Prefabs/Skeleton.tscn").instantiate()
+	#Once first threshold reached you can spawn zombies too
+	elif next_threshold_cap <= 5000:
+		var enemy_rand = randi() % 2
+		match enemy_rand:
+			0:
+				return load("res://Assets/Prefabs/Zombie.tscn").instantiate()
+			1: 
+				return load("res://Assets/Prefabs/Skeleton.tscn").instantiate()
+	#Once past second threshold we can spawn all three
+	else:
+		var enemy_rand = randi() % 3
+		match enemy_rand:
+			0:
+				return load("res://Assets/Prefabs/Zombie.tscn").instantiate()
+			1: 
+				return load("res://Assets/Prefabs/Skeleton.tscn").instantiate()
+			2:
+				return load("res://Assets/Prefabs/Wraith.tscn").instantiate()
