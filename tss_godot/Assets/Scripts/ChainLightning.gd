a13addeed6e9e1809e02d22d7e4966c9f4d85609
Shadowyish <Shadowyish0@gmail.com>
2025-04-12 15:27:38 -0400

    Chain Lightning works!

--- tss_godot/Assets/Scripts/ChainLightning.gd
+++ tss_godot/Assets/Scripts/ChainLightning.gd
@@ -8,6 +8,7 @@ extends Node2D
 var fired_angle: float # angle for bolt to travel
 var has_hit = false
 var hit_targets: Array
+var next_target: CharacterBody2D
 
 @onready var destroy_timer = Timer.new()
 @onready var max_radius = $Radius
@@ -24,37 +25,44 @@ func _ready():
 	rotation = fired_angle + (PI/2)
 
 func _physics_process(delta):
-	if !has_hit:
-		position += Vector2.RIGHT.rotated(fired_angle) * speed * delta
+	if has_hit: 
+		if is_instance_valid(next_target):
+			fired_angle = (next_target.global_position - global_position).normalized().angle()
+	position += Vector2.RIGHT.rotated(fired_angle) * speed * delta 
 
 func _on_bolt_timeout():
 	queue_free()
 
 func _on_body_entered(body):
-	if not body.is_in_group("Player"):
-		destroy_timer.stop()
-		hit_targets = [body]
-		has_hit = true
-		anim.visible = false
-		chain()
+	if !body.is_in_group("Player") and !(body in hit_targets):
+		if !has_hit:
+			destroy_timer.stop()
+			hit_targets = [body]
+			has_hit = true
+			$LightningTrail.emitting = true
+			$LightningSparks.emitting = true
+		else: 
+			hit_targets.append(body)
+			$LightningSparks.emitting = true
+		body.take_damage(damage)
+		max_jumps -= 1
+		if max_jumps <= 1:
+			queue_free()
+		damage = max(damage/2, 1)
+		next_target = chain()
 
 func chain():
-	for i in range(max_jumps):
-		var last_target = hit_targets[-1]
-		var new_target = find_closest_enemy_not_in(hit_targets, last_target.global_position)
-		if new_target:
-			hit_targets.append(new_target)
-			new_target.take_damage(damage)
-			damage = max(int(damage/2), 1)
-			animate_lightning(last_target.global_position, new_target.global_position)
-			global_position = new_target.global_position
-		else:
-			break
-	queue_free()
+	var new_target = find_closest_enemy_not_in(hit_targets, global_position)
+	if new_target:
+		return new_target
+	else:
+		queue_free()
 	
 func find_closest_enemy_not_in(targets_to_ignore: Array, start_pos: Vector2)-> CharacterBody2D: 
 	var closest_body: CharacterBody2D
 	for body in max_radius.get_overlapping_bodies():
+		if body.is_in_group("Player"):
+			continue
 		if body.has_method("take_damage") and not body in targets_to_ignore:
 			if not closest_body:
 				closest_body = body
@@ -62,18 +70,3 @@ func find_closest_enemy_not_in(targets_to_ignore: Array, start_pos: Vector2)-> C
 				if start_pos.distance_to(closest_body.global_position) > start_pos.distance_to(body.global_position):
 					closest_body = body
 	return closest_body
-	
-func animate_lightning(start_pos: Vector2, end_pos: Vector2):
-	var lightning = Line2D.new()
-	var segment_count = 8
-	lightning.width = 2
-	lightning.default_color = Color.WHITE
-	lightning.points = []
-	for i in range(segment_count + 1):
-		var t = i / segment_count
-		var pos = start_pos.lerp(end_pos, segment_count)
-		if i != 0 and i != segment_count:
-			pos += Vector2(randf() - 0.5, randf() - 0.5)
-		lightning.add_point(lightning.to_local(pos))
-	get_tree().current_scene.add_child(lightning)
-	lightning.queue_free()
